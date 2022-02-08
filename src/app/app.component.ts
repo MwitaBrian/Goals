11538cd24badfe22028bd142be4a31893703bf29
MwitaBrian <brianmwita13@gmail.com>
2022-02-08 12:24:17 +0300

    Move goals Array from AppComponent class to GoalComponent class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,6 @@ import { Goal } from './goal';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals:Goal[] = [
-    {id:1, name:'Watch finding Nemo'},
-    {id:2, name:'Watch Titanic'},
-    {id:3, name:'Buy Cookies'},
-    {id:4, name:'Get new Phone Case'},
-    {id:5, name:'Get Dog Food'},
-    {id:6, name:'Solve math homework'},
-    {id:7, name:'Plot my world domination plan'},
-  ]
+  
 
 }
--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Goal } from '../goal';
 
 @Component({
   selector: 'app-goal',
@@ -6,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./goal.component.css']
 })
 export class GoalComponent implements OnInit {
+
+  goals:Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2, name:'Watch Titanic'},
+    {id:3, name:'Buy Cookies'},
+    {id:4, name:'Get new Phone Case'},
+    {id:5, name:'Get Dog Food'},
+    {id:6, name:'Solve math homework'},
+    {id:7, name:'Plot my world domination plan'},
+  ];
 
   constructor() { }
 
